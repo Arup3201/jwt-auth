c7f2a120ab7e17fea920f20e2003d60a309d4c2e
Arup3201 <arupjana7365@gmail.com>
2026-03-25 22:44:41 +0530

    update: jwt util added

--- backend/utils/auth.go
+++ backend/utils/auth.go
@@ -6,14 +6,98 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"time"
 
 	"example.com/go-jwt-auth/models"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 const (
 	USER_KEY = "USER"
+
+	JWT_ALG_ECDSA   = "ECDSA"
+	JWT_ALG_ED25519 = "ED25519"
+	JWT_ALG_HMAC    = "HMAC"
+	JWT_ALG_RSA     = "RSA"
 )
+
+type JWTClaims struct {
+	Issuer    string    `json:"iss"`
+	UserId    string    `json:"user_id"`
+	Subject   string    `json:"sub"`
+	NotBefore time.Time `json:"nbf"`
+	IssuedAt  time.Time `json:"iat"`
+	Expiry    time.Time `json:"exp"`
+}
+
+func NewClaims(iss, sub, userId string, exp time.Time) *JWTClaims {
+	now := time.Now().UTC()
+
+	return &JWTClaims{
+		Issuer:    iss,
+		Subject:   sub,
+		UserId:    userId,
+		IssuedAt:  now,
+		NotBefore: now,
+		Expiry:    exp,
+	}
+}
+
+type CustomClaims struct {
+	UserId string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+type JWT struct {
+	Claims *JWTClaims
+	Alg    jwt.SigningMethod
+}
+
+func JWTFromClaims(claims *JWTClaims,
+	alg string) (*JWT, error) {
+
+	obj := JWT{
+		Claims: claims,
+	}
+
+	switch alg {
+	case JWT_ALG_ECDSA:
+		obj.Alg = jwt.SigningMethodES256
+	case JWT_ALG_ED25519:
+		obj.Alg = jwt.SigningMethodEdDSA
+	case JWT_ALG_HMAC:
+		obj.Alg = jwt.SigningMethodHS256
+	case JWT_ALG_RSA:
+		obj.Alg = jwt.SigningMethodRS256
+	default:
+		return nil, errors.New("invalid jwt algorithm")
+	}
+
+	return &obj, nil
+}
+
+func (t *JWT) Sign(key string) (string, error) {
+
+	claims := CustomClaims{
+		UserId: t.Claims.UserId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(t.Claims.Expiry),
+			IssuedAt:  jwt.NewNumericDate(t.Claims.IssuedAt),
+			NotBefore: jwt.NewNumericDate(t.Claims.NotBefore),
+			Issuer:    t.Claims.Issuer,
+			Subject:   t.Claims.Subject,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, err := token.SignedString([]byte(key))
+	if err != nil {
+		return "", fmt.Errorf("jwt sign: %w", err)
+	}
+
+	return signed, nil
+}
 
 // NewContext returns a new Context that carries value u.
 func NewContext(ctx context.Context, u models.User) context.Context {
---
+++ backend/utils/auth_test.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"testing"
+	"time"
+
+	jwtv5 "github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewClaims(t *testing.T) {
+	var iss, sub, uId string
+	var exp time.Time
+	iss = "https://auth.go.com"
+	sub = "sample_sub"
+	uId = "123"
+	exp = time.Now().Add(10 * time.Second)
+
+	claims := NewClaims(iss, sub, uId, exp)
+
+	assert.NotNil(t, claims)
+	assert.Equal(t, claims.Issuer, iss)
+	assert.Equal(t, claims.Subject, sub)
+	assert.Equal(t, claims.UserId, uId)
+	assert.Equal(t, claims.Expiry, exp)
+}
+
+func TestJWTFromClaims(t *testing.T) {
+	var iss, sub, uId string
+	var exp time.Time
+	iss = "https://auth.go.com"
+	sub = "sample_sub"
+	uId = "123"
+	exp = time.Now().Add(10 * time.Second)
+	claims := NewClaims(iss, sub, uId, exp)
+
+	jwt, err := JWTFromClaims(claims, JWT_ALG_RSA)
+
+	assert.NoError(t, err)
+	assert.Equal(t, jwtv5.SigningMethodRS256, jwt.Alg)
+	assert.Equal(t, "https://auth.go.com", jwt.Claims.Issuer)
+}
+
+func TestJWTSign(t *testing.T) {
+	var iss, sub, uId string
+	var exp time.Time
+	iss = "https://auth.go.com"
+	sub = "sample_sub"
+	uId = "123"
+	exp = time.Now().Add(10 * time.Second)
+	claims := NewClaims(iss, sub, uId, exp)
+	jwt, _ := JWTFromClaims(claims, JWT_ALG_RSA)
+	secret := "secret"
+
+	tokenStr, err := jwt.Sign(secret)
+
+	assert.NoError(t, err)
+
+	token, err := jwtv5.ParseWithClaims(tokenStr, &CustomClaims{}, func(token *jwtv5.Token) (any, error) {
+		return []byte(secret), nil
+	})
+	assert.NoError(t, err)
+	if claims, ok := token.Claims.(*CustomClaims); ok {
+		assert.Equal(t, uId, claims.UserId)
+		assert.Equal(t, sub, claims.Subject)
+		assert.Equal(t, iss, claims.Issuer)
+	}
+}
