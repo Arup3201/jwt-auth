280029a607a198187ce046e46032c47809a7782d
Arup3201 <arupjana7365@gmail.com>
2026-03-27 17:03:40 +0530

    refactor: remove unnecessary code

--- backend/main.go
+++ backend/main.go
@@ -104,28 +104,6 @@ func GenerateAndSaveKeys() (RSAKeys, error) {
 	return keys, nil
 }
 
-func LoadPrivateKey(path string) (*rsa.PrivateKey, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	block, _ := pem.Decode(data)
-	return x509.ParsePKCS1PrivateKey(block.Bytes)
-}
-
-func LoadPublicKey(path string) (*rsa.PublicKey, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	block, _ := pem.Decode(data)
-	key, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	return key.(*rsa.PublicKey), nil
-}
-
 func main() {
 	var err error
 	var db *gorm.DB
